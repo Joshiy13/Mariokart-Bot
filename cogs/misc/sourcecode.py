1803f3ef144bc00e5a725b36c8e4f826ad3ba570
Joshua Weidanz <joshua@weidanz.de>
2024-12-05 12:52:13 +0100

    fixed /sourcecode

--- cogs/misc/sourcecode.py
+++ cogs/misc/sourcecode.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-class Ping(commands.Cog):
+class Sourcecode(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
@@ -9,9 +9,9 @@ class Ping(commands.Cog):
 
     @commands.slash_command(name="sourcecode", description="Shows the Source Code of the bot")
     async def ping(self, ctx):
-        ping = discord.Embed(title=f"Source Code: https://github.com/Joshiy13/Mariokart-Bot", color=0xfa0505)
-        await ctx.respond(embed = ping)
+        code = discord.Embed(title=f"Source Code: https://github.com/Joshiy13/Mariokart-Bot", color=0xfa0505)
+        await ctx.respond(embed = code)
 
 
 def setup(bot): 
-    bot.add_cog(Ping(bot))
+    bot.add_cog(Sourcecode(bot))
